e8bde1ee12c617e650dc3605fac7082a67bbc8fc
Eddie Colon <coloed3@gmail.com>
2021-01-09 19:46:05 -0700

    front of first commit

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FourohFourPage() {
+  return (
+    <div>
+      <p> testing this </p>
+    </div>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+function HomePage() {
+  return (
+    <>
+      <p> Becomeing the boss at gatsby </p>
+    </>
+  );
+}
+
+export default HomePage;
